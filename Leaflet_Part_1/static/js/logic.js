c804fd3c6b4ba3832d532c88f3c48818bcbb42bc
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 <Shannon>
2024-07-16 21:26:36 -0500

    final homework commit

--- Leaflet_Part_1/static/js/logic.js
+++ Leaflet_Part_1/static/js/logic.js
@@ -11,3 +11,83 @@ var myMap = L.map("map", {
     attribution: "© OpenStreetMap contributors"
   }).addTo(myMap);
   
+  // URL to fetch earthquake data for the last 7 days
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {
+  // send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+  // Define a function for each feature in the array
+  // Give each feature a popup - place and time
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
+      "<hr><p>Magnitude: " + feature.properties.mag + "</p>");
+  }
+
+  // circle markers based on magnitude
+  function createCircleMarker(feature, latlng) {
+    let options = {
+      radius: feature.properties.mag * 5,
+      fillColor: getColor(feature.properties.mag),
+      color: "black",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    };
+    return L.circleMarker(latlng, options);
+  }
+
+  // GeoJSON layer containing the features array on the earthquakeData object
+  // onEachFeature function once for each piece of data in the array
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: createCircleMarker
+  });
+
+  // Add the earthquakes layer to the map
+  earthquakes.addTo(myMap);
+}
+
+// Function to determine marker color based on earthquake magnitude
+function getColor(magnitude) {
+  return magnitude > 5 ? '#F06B6B' :
+         magnitude > 4 ? '#F0A76B' :
+         magnitude > 3 ? '#F3BA4D' :
+         magnitude > 2 ? '#F3DB4D' :
+         magnitude > 1 ? '#E1F34D' :
+                         '#B7F34D';
+}
+// Create a legend (using Leaflet's "L.control")
+// the 'onAdd' creates a div and populates it with colored squares
+// representing depth.
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create('div', 'info legend');
+  var grades = [-10, 10, 30, 50, 70, 90];
+  var colors = [
+    '#A3F600',
+    '#DCF400',
+    '#F7DB11',
+    '#FDB72A',
+    '#FCA35D',
+    '#FF5F65'
+  ];
+
+  // Loop through our depth intervals and generate a label with a colored square for each interval
+  for (var i = 0; i < grades.length; i++) {
+    div.innerHTML +=
+      '<i style="background:' + colors[i] + '"></i> ' +
+      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+
+  return div;
+};
+
+// Add the legend to the map
+legend.addTo(myMap);
--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # leaflet_challenge
+Shannon Hoffman
+
+# Description
 The USGS is interested in building a new set of tools that will allow them to visualize their earthquake data. They collect a massive amount of data from all over the world each day, but they lack a meaningful way of displaying it. Develop a way to visualize USGS data.
 
-# Part 1: 
+# Instructions: 
+Part 1 required, Part 2 optional.
 Initialize using Leaflet and add a base layer (OpenStreetMap - like in class)
 Fetch the data (using D3)
 Plot the earthquake data
 Use popups
+
+Collected USGS data from, chose earthquake data from the "last 7 days"
+
+# Resources
+Instructors - Tom Bogue and Jordan Tompkin
+Classmates
+ChatGPT and Xpert Learning Assistant
+USGS earthquake dataset: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
